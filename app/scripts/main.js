686ab4b920df4f6692cc96abb850e55c60c462db
Michael Hardin <mhardinla@gmail.com>
2016-02-13 15:59:24 -0800

    final commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,12 @@ require.config({
     'jquery': 'vendor/jquery/dist/jquery',
     'underscore': 'vendor/underscore-amd/underscore',
     'backbone': 'vendor/backbone-amd/backbone',
-  }
+    hbs: 'vendor/require-handlebars-plugin/hbs',
+  },
+
+  hbs: {
+      templateExtension: 'html',
+  },
 });
 
 require(['views/app'], function(AppView) {
--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -1,7 +1,25 @@
-define(['backbone'], function(Backbone) {
+define([
+    'backbone',
+    'hbs!templates/form',
+], function(Backbone, FormTemp) {
   var App = Backbone.View.extend({
+
+    el: 'body',
+
+    events: {
+        'click #nextForm': 'getNextForm',
+    },
+
     initialize: function() {
       console.log( 'Wahoo!' );
+    },
+
+    getNextForm: function(){
+        var el = $('.form-box-inputs');
+        $('.back').show();
+        el.empty();
+        el.html(FormTemp());
+
     }
   });
 
